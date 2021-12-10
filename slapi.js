869e2af39851c3d1dd78cb885ac0b9ef9b27361c
tjbleivns <tjblevins@gmail.com>
2021-12-09 18:12:10 -0600

    Update All

--- slapi.js
+++ slapi.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const app = require('node-screenlogic-rest/app');
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 const server = http.createServer(app);
 
